146209e7471916d2f1a8269575b6abc579fb1ae5
Ben <benjamin.fulton99@gmail.com>
2021-06-08 14:44:03 -0600

    Creates relationships between tea and subs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_203418) do
+ActiveRecord::Schema.define(version: 2021_06_08_203957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2021_06_08_203418) do
     t.string "frequency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "tea_id"
+    t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
   end
 
   create_table "teas", force: :cascade do |t|
@@ -42,4 +44,5 @@ ActiveRecord::Schema.define(version: 2021_06_08_203418) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "subscriptions", "teas"
 end
